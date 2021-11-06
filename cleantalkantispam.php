5565f00cd61ed967a0988c51d1ac89847e671dbc
Artem Anoshin <artem-anoshin@yandex.ru>
2021-11-06 19:00:16 +0400

    ewrvrev

--- cleantalkantispam.php
+++ cleantalkantispam.php
@@ -460,16 +460,22 @@ class plgSystemCleantalkantispam extends JPlugin
         
         // Updating roles_exclusion
         $excluded_roles = $this->params->get('roles_exclusions');
+		
+		$log = print_r($excluded_roles, true);
+		file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+
         if (is_array($excluded_roles)) {
             $default_roles = self::getGroups();
             $new_data_roles_excluded = array();
-
+			$log = print_r($default_roles, true);
+			file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
             foreach ($default_roles as $default_role) {
                 if (in_array(strtolower($default_role->id), $excluded_roles)) {
                     $new_data_roles_excluded[] = strtolower($default_role->title);
                 }
             }
-
+			$log = print_r($new_data_roles_excluded, true);
+			file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
             $this->params->set('roles_exclusions', implode(',', $new_data_roles_excluded));
 
             $db = JFactory::getDbo();
@@ -480,6 +486,8 @@ class plgSystemCleantalkantispam extends JPlugin
             $query->where($db->quoteName('folder') . ' = ' . $db->quote('system'));
             $db->setQuery($query);
             $db->execute();
+			$log = print_r($db, true);
+			file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
         }
     }
 
