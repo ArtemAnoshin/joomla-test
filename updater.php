8257ba4474ced9969899b60300b18f94d7438bd6
Artem Anoshin <artem-anoshin@yandex.ru>
2021-11-09 11:30:29 +0400

    erferf

--- updater.php
+++ updater.php
@@ -38,37 +38,39 @@ class plgsystemcleantalkantispamInstallerScript
     }
     public function update($parent)
     {
-        // Updating roles_exclusion
-        $excluded_roles = $this->params->get('roles_exclusions');
-
-        $log = print_r($excluded_roles, true);
+        $log = print_r($parent, true);
         file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
-
-        if (is_array($excluded_roles)) {
-            $default_roles = self::getGroups();
-            $new_data_roles_excluded = array();
-            $log = print_r($default_roles, true);
-            file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
-            foreach ($default_roles as $default_role) {
-                if (in_array(strtolower($default_role->id), $excluded_roles)) {
-                    $new_data_roles_excluded[] = strtolower($default_role->title);
-                }
-            }
-            $log = print_r($new_data_roles_excluded, true);
-            file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
-            $this->params->set('roles_exclusions', implode(',', $new_data_roles_excluded));
-
-            $db = JFactory::getDbo();
-            $query = $db->getQuery(true);
-            $query->clear()->update($db->quoteName('#__extensions'));
-            $query->set($db->quoteName('params') . '= ' . $db->quote((string) $this->params));
-            $query->where($db->quoteName('element') . ' = ' . $db->quote('cleantalkantispam'));
-            $query->where($db->quoteName('folder') . ' = ' . $db->quote('system'));
-            $db->setQuery($query);
-            $db->execute();
-            $log = print_r($db, true);
-            file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
-        }
+        // Updating roles_exclusion
+//        $excluded_roles = $this->params->get('roles_exclusions');
+//
+//        $log = print_r($excluded_roles, true);
+//        file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+//
+//        if (is_array($excluded_roles)) {
+//            $default_roles = self::getGroups();
+//            $new_data_roles_excluded = array();
+//            $log = print_r($default_roles, true);
+//            file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+//            foreach ($default_roles as $default_role) {
+//                if (in_array(strtolower($default_role->id), $excluded_roles)) {
+//                    $new_data_roles_excluded[] = strtolower($default_role->title);
+//                }
+//            }
+//            $log = print_r($new_data_roles_excluded, true);
+//            file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+//            $this->params->set('roles_exclusions', implode(',', $new_data_roles_excluded));
+//
+//            $db = JFactory::getDbo();
+//            $query = $db->getQuery(true);
+//            $query->clear()->update($db->quoteName('#__extensions'));
+//            $query->set($db->quoteName('params') . '= ' . $db->quote((string) $this->params));
+//            $query->where($db->quoteName('element') . ' = ' . $db->quote('cleantalkantispam'));
+//            $query->where($db->quoteName('folder') . ' = ' . $db->quote('system'));
+//            $db->setQuery($query);
+//            $db->execute();
+//            $log = print_r($db, true);
+//            file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+//        }
     }
     public function uninstall($parent)
     {
